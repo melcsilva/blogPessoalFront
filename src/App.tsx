afe5a6972c4d01f1a7e52605089aa15e6c3da2b6
Melissa <melissasilva245@gmail.com>
2025-08-01 18:35:32 -0300

    implementação do CRUD de postagens e perfil no blog

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Footer from './components/footer/Footer'
 import Navbar from './components/navbar/Navbar'
-import ListaTemas from './components/temas/listatemas/ListaTemas'
-import FormTema from './components/temas/formtema/FormTema'
+import ListaPostagens from './components/postagens/listapostagem/ListaPostagens'
 import DeletarTema from './components/temas/deletartema/DeletarTema'
+import DeletarPostagem from './components/postagens/deletarpostagem/DeletarPostagem'
+import FormTema from './components/temas/formtema/FormTema'
+import ListaTemas from './components/temas/listatemas/ListaTemas'
+import FormPostagem from './components/postagens/formpostagem/FormPostagem'
 import { AuthProvider } from './contexts/AuthContext'
+import Cadastro from './pages/cadastro/Cadastro'
 import Home from './pages/home/Home'
 import Login from './pages/login/Login'
-import Cadastro from './pages/cadastro/Cadastro'
+import Perfil from './pages/perfil/Perfil'
 
 function App() {
 return (
@@ -26,6 +30,11 @@ return (
             <Route path="/cadastrartema" element={<FormTema />} />
             <Route path="/editartema/:id" element={<FormTema />} />
             <Route path="/deletartema/:id" element={<DeletarTema />} />
+            <Route path="/postagens" element={<ListaPostagens />} />
+            <Route path="/cadastrarpostagem" element={<FormPostagem />} />
+            <Route path="/editarpostagem/:id" element={<FormPostagem />} />
+            <Route path="/deletarpostagem/:id" element={<DeletarPostagem />} />
+            <Route path="/perfil" element={<Perfil />} />
             </Routes>
         </div>
         <Footer />
