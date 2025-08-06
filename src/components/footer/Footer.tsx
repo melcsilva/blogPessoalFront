fe2abe132ef50b0d15d1aa4f1f56d814298c3919
Melissa <melissasilva245@gmail.com>
2025-08-06 11:22:16 -0300

    ajustando navbar e footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,30 +1,44 @@
 import { GithubLogoIcon, InstagramLogoIcon, LinkedinLogoIcon } from '@phosphor-icons/react'
+import { ReactNode, useContext } from 'react'
+import { AuthContext } from '../../contexts/AuthContext'
 
 function Footer() {
 
     let data = new Date().getFullYear()
 
-    return (
-        <>
+    const { usuario } = useContext(AuthContext)
+
+    let component: ReactNode
+
+    if (usuario.token !== "") {
+
+        component = (
+
             <div className="flex justify-center bg-indigo-900 text-white">
                 <div className="container flex flex-col items-center py-4">
                     <p className='text-xl font-bold'>
-                            Blog Pessoal Mel | Copyright: {data}
-                        </p>
-                    <p className='text-lg'>Acesse minhas redes sociais</p>
+                        Blog Pessoal Generation | Copyright: {data}
+                    </p>
+                    <p className='text-lg'>Acesse nossas redes sociais</p>
                     <div className='flex gap-2'>
-                        <a href="https://www.linkedin.com/in/melissa-cassia-41588921b/" target="_blank">
-                        <LinkedinLogoIcon size={48} weight='bold' />
+                        <a href="https://www.linkedin.com/school/generationbrasil" target="_blank">
+                            <LinkedinLogoIcon size={48} weight='bold' />
                         </a>
-                        <a href="https://www.instagram.com/meli.cs" target="_blank">
-    	                        <InstagramLogoIcon size={48} weight='bold' />
+                        <a href="https://www.instagram.com/generationbrasil" target="_blank">
+                            <InstagramLogoIcon size={48} weight='bold' />
                         </a>
                         <a href="https://github.com/melcsilva" target="_blank">
-    	                        <GithubLogoIcon size={48} weight='bold' />
-	                    </a>
+                        <GithubLogoIcon size={48} weight='bold' />
+                        </a>
                     </div>
                 </div>
             </div>
+
+        )
+    }
+    return (
+        <>
+            { component }
         </>
     )
 }
